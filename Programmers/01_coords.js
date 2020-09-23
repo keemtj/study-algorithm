cb33b07fd8b7e2688b2cb54d8cd3b943f5816d18
keemtj <keemgreat@gmail.com>
2020-09-23 18:02:07 +0900

    :construction: 01. solution2 of coords.js

--- Programmers/01_coords.js
+++ Programmers/01_coords.js
@@ -17,8 +17,22 @@ function solution(v) {
       (value, i, arr) =>
         arr.indexOf(value) === i && arr.indexOf(value, i + 1) === -1,
     );
-  console.log([...coordX, ...coordY]);
+  // console.log([...coordX, ...coordY]);
   return [...coordX, ...coordY];
+}
+
+function solution2(v) {
+  const xCoords = v.map(c => c[0]);
+  const yCoords = v.map(c => c[1]);
+
+  const x = xCoords.find(
+    (value, i, arr) => arr.indexOf(value) === arr.lastIndexOf(value),
+  );
+  const y = yCoords.find(
+    (value, i, arr) => arr.indexOf(value) === arr.lastIndexOf(value),
+  );
+  console.log([x, y]);
+  return [x, y];
 }
 
 solution([
@@ -46,5 +60,8 @@ solution([
  * 풀이
  * 1. Array.prototype.map: 주어진 배열에서 x좌표만 추출
  * 2. Array.prototype.filter: x좌표만 담겨있는 배열에서 중복되지 않는 x좌표만 추출
- * 3. Array.prototype.indexOf: 원본배열의 각각의 요소를 검색하여 인수로 전달된 value와 일치하는 첫번째 요소의 index를 반환한다
+ * 3-1. Array.prototype.indexOf: 원본배열의 각각의 요소를 검색하여 인수로 전달된 value와 일치하는 첫번째 요소의 index를 반환한다
+ * -> 일치하는 요소가 없으면 -1을 반환한다
+ * 3-2. Array.prototype.lastIndexOf: 원본 배열의 각각의 요소를 마지막부터 검색하여 일치하는 첫번째 요소의 index를 반환한다
+ * -> 일치하는 요소가 없으면 -1을 반환한다
  */
