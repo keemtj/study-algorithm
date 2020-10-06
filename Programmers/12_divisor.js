bdc6b485c2f2484df701b3eb40e3f2f0b6b0ef91
keemtj <keemgreat@gmail.com>
2020-10-06 22:43:13 +0900

    :bulb: Add comments about solution

--- Programmers/12_divisor.js
+++ Programmers/12_divisor.js
@@ -13,3 +13,10 @@ function solution(arr, divisor) {
 
 solution([5, 9, 7, 10], 5); // [5, 10]
 solution([3, 2, 6], 10); // [-1]
+
+/**
+ * 풀이
+ * 1. Array.prototype.filter(): callback함수의 값이 true인 요소만 반환하여 새로운 배열 생성
+ * 2. Array.prototype.sort(): 기본적으로 오름차순정렬을 한다. 단, 숫자 요소의 배열의 경우 유니코드 코드 포인트에 따라 정렬하므로 주의가 필요하다
+ *                            sort((a, b) => a - b), sort((a, b) => b - a)
+ */
