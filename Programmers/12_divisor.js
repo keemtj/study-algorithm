234c457d981c741d270518afa4fb0b6e2e937b46
keemtj <keemgreat@gmail.com>
2020-10-06 14:31:21 +0900

    :pencil: 12. divisor.js

---
+++ Programmers/12_divisor.js
@@ -0,0 +1,15 @@
+/**
+ * 나누어 떨어지는 숫자 배열
+ * array의 각 element 중 divisor로 나누어 떨어지는 값을 오름차순으로 정렬한 배열을 반환
+ * divisor로 나누어 떨어지는 element가 하나도 없다면 배열에 -1을 담아 반환
+ * 정수 i, j에 대해 i * j이면 arr[i]*arr[j]이다
+ */
+
+function solution(arr, divisor) {
+  const answer = arr.filter(v => !(v % divisor)).sort((a, b) => a - b);
+  if (!answer.length) return [-1];
+  return answer;
+}
+
+solution([5, 9, 7, 10], 5); // [5, 10]
+solution([3, 2, 6], 10); // [-1]
