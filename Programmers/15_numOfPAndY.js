17a5694d782b4011f50410f6dcbf3e416ac5b4f7
keemtj <keemgreat@gmail.com>
2020-10-09 00:13:57 +0900

    :construction: Fix return value and Add comments

--- Programmers/15_numOfPAndY.js
+++ Programmers/15_numOfPAndY.js
@@ -1,4 +1,5 @@
 /**
+ * ! 해결
  * 문자열 내 p와 y의 개수
  * 대소문자 구분없이 p,y의 개수를 비교해 같으면 true, 다르면 false
  * 둘다 존재하지 않을 경우 true
@@ -8,8 +9,14 @@ function solution(s) {
   const string = s.toLowerCase();
   const p = [...string].filter(str => str === 'p').length;
   const y = [...string].filter(str => str === 'y').length;
-  console.log(p >= y);
-  return p >= y;
+  return p === y ? true : false;
 }
 
 solution('pPoooyY');
+
+/**
+ * 풀이
+ * 1. String.prototype.toLowerCase(): 호출된 문자열을 소문자로 변환하여 반환
+ * 2. 스프레드 연산자
+ * 3. Array.prototype.filter()
+ */
