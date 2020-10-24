3f013b1a698b4f23cf40480f2cd2d7fcf3e120a9
keemtj <keemgreat@gmail.com>
2020-10-24 20:32:38 +0900

    :fire: remove code

--- Programmers/03_failureRate.js
+++ Programmers/03_failureRate.js
@@ -1,18 +1,9 @@
 /**
- * ! 미해결
- * 2019 KAKAO BLIND RECRUITMENT - 실패율
- * @param N stage 개수
- * @param stages stages.length = users.length
- * 1 <= stages value <= N + 1(=마지막 스테이지 N까지 클리어한 사용자)
- * 실패율 = 스테이지에 도달했으나 아직 클리어하지 못한 플레이어의 수 / 스테이지에 도달한 플레이어 수
- * 실패율의 내림차순
- * 실패율이 같은 경우 작은 번호의 스테이지가 먼저 오도록 오름차순
+ * 실패율 (2019 KAKAO BLIND RECRUITMENT)
+ * 실패율: 스테이지에 도달했으나 아직 클리어하지 못한 플레이어의 수 / 스테이지에 돋달한 플레이어의 수
+ * 전체스테이지 N, 게임을 이용하는 사용자가 멈춰있는 스테이지의 번호가 담긴 배열 stages
+ * stages에는 1 ~ N + 1의 자연수가 담겨있다
+ * 실패율이 높은 스테이지부터 내림차순으로 리턴
  */
-function solution(N, stages) {
-  console.log(N, stages);
-  const answer = [];
-  // let failureRate = stage1 / stage1.length;
-  return answer;
-}
 
 solution(5, [2, 1, 2, 6, 2, 4, 3, 3]); // 실패율 = [3, 4, 2, 1, 5];
