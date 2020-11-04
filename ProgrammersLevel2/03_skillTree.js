95ba230a826e8d89983fe4e09bc02cd260e53fc5
keemtj <keemgreat@gmail.com>
2020-11-04 23:06:15 +0900

    :seedling: Add seed file

---
+++ ProgrammersLevel2/03_skillTree.js
@@ -0,0 +1,11 @@
+/**
+ * summer/winter coding (2018)
+ * 스킬트리
+ */
+
+function solution(skill, skill_trees) {
+  var answer = 0;
+  return answer;
+}
+
+solution('CBD', ['BACDE', 'CBADF', 'AECB', 'BDA']); // 2
