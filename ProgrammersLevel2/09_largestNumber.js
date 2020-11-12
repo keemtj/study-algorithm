0ec9cb5a987335d162fd7010a779d0feeae2b2d4
keemtj <keemgreat@gmail.com>
2020-11-13 07:00:22 +0900

    :pencil: 09. Largest number

--- ProgrammersLevel2/09_largestNumber.js
+++ ProgrammersLevel2/09_largestNumber.js
@@ -9,7 +9,61 @@
  */
 
 function solution(numbers) {
-  return 0;
+  const result = numbers
+    .map(v => String(v))
+    // .map(v => v + '') // 다른 사람 풀이, js의 타입변환
+    .sort((a, b) => {
+      if (a + b > b + a) {
+        return -1;
+      } else if (a + b < b + a) {
+        return 1;
+      } else {
+        return 0;
+      }
+    })
+    .join('');
+  // const answer = String(parseInt(result, 10));
+  if (result[0] === '0') return '0';
+  return result;
 }
 
-solution([3, 30, 34, 5, 0]);
+solution([0, 0, 0]);
+solution([0, 10, 0]);
+solution([6, 10, 2]);
+solution([3, 30, 34, 5, 9]);
+
+function solution2(numbers) {
+  const answer = numbers
+    .map(v => String(v))
+    .sort((a, b) => (b + a).localeCompare(a + b))
+    .join('');
+  if (answer[0] === '0') return '0';
+  return answer;
+}
+
+solution2([0, 0, 0]);
+solution2([0, 10, 0]);
+solution2([6, 10, 2]);
+solution2([3, 30, 34, 5, 9]);
+
+/**
+ * 풀이
+ * sort() 메서드의 파라미터 compareFn의 원리에 대해서 제대로 공부하자...
+ */
+
+/**
+ * 맨 처음 풀이... 풀다가 잘못 생각했다.
+ */
+function solution3(numbers) {
+  const answer = numbers
+    .map(v => [...String(v)])
+    .flat()
+    .sort((a, b) => b - a)
+    .join('');
+  return answer;
+}
+
+solution3([0, 0, 0]);
+solution3([0, 10, 0]);
+solution3([6, 10, 2]);
+solution3([3, 30, 34, 5, 9]);
