739214ff4a228078b31e4585d06eece329deeb1e
keemtj <keemgreat@gmail.com>
2020-11-18 12:29:46 +0900

    :seedling: Add seed file

---
+++ ProgrammersLevel2/13_H-index.js
@@ -0,0 +1,17 @@
+/**
+ * H-index
+ * (정렬)
+ * 과학자의 생산성과 영향력을 나타내는 지표
+ * 어느 과학자의 H_index를 나타내는 값인 h를 구하려고한다
+ *
+ * 어떤 과학자가 발표한 논문 n편, h번 이상 인용된 논문이 h편 이상이고 나머지 논문이 h번 이하 인용
+ * h의 최댓값이 이 과학자의 h-index
+ * 논문인용횟수를 담은 배열 citations 일때 h를 리턴
+ */
+
+function solution(citations) {
+  let h = 0;
+  return 0;
+}
+
+solution([3, 0, 6, 1, 5]); // 3
