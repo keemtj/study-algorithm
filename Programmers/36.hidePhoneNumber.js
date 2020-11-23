07a2521052e94cc3b82f2b6fe54c869d61c6581b
keemtj <keemgreat@gmail.com>
2020-11-24 03:22:12 +0900

    :construction: Add other solution

--- Programmers/36.hidePhoneNumber.js
+++ Programmers/36.hidePhoneNumber.js
@@ -3,17 +3,29 @@
  * 전화번호 문자열의 맨 뒷자리 4자리를 제외한 나머지 숫자를 전부 '*'로 가린 문자열 리턴
  */
 
-function solution(phone_number) {
-  const hidden_number = [...phone_number]
-    .map((v, i, arr) => (i >= arr.length - 4 ? v : '*'))
-    .join('');
-  console.log(hidden_number);
-  return hidden_number;
-}
+// function solution(phone_number) {
+//   const hidden_number = [...phone_number]
+//     .map((v, i, arr) => (i >= arr.length - 4 ? v : '*'))
+//     .join('');
+//   console.log(hidden_number);
+//   return hidden_number;
+// }
 
-solution('027778888'); // '*******4444'
-solution('01064154738'); // '*******4444'
-solution('010641547389999'); // '*******4444'
+// function solution(phoneNumber) {
+//   const last4Digits = phoneNumber.slice(-4);
+//   console.log(last4Digits);
+//   const answer = last4Digits.padStart(phoneNumber.length, '*');
+//   console.log(answer);
+//   return answer;
+// }
+
+function solution(phoneNumber) {
+  const last4Digits = phoneNumber.slice(-4);
+  const answer = '*'.repeat(phoneNumber.length - 4) + last4Digits;
+  console.log(answer);
+  return answer;
+}
+solution('0277778888'); // '******8888'
 
 /**
  * 풀이
